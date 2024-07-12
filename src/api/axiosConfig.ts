4db72edd48a910537e5310b7b1de86fb6c374e38
siham <siham.heddi.dev@gmail.com>
2024-07-12 10:51:55 +0100

    change baseurl of api

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3000/api/v1";
+export const BASE_URL = "https://skin-care-rest-api.onrender.com/api/v1";
 export const axiosConfig = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: "https://skin-care-rest-api.onrender.com",
 })
 
 
